1c59e378d1aca0fae2b063496b013c4c72ec2744
Mim <mimworkmail@gmail.com>
2015-02-22 17:47:09 +0300

    Исправлен расчёт средних цен

--- pyramid/ensemplix/sql/item_stats.sql
+++ pyramid/ensemplix/sql/item_stats.sql
@@ -1,6 +1,6 @@
 SELECT
-	operation, count(*), sum(amount), sum(price),
-	min(1.*price/amount), avg(1.*price/amount), max(1.*price/amount)
+	operation, count(*), sum(amount) AS sum_amount, sum(price) AS sum_price,
+	min(1.*price/amount) AS min_price, 1.*sum(price)/sum(amount) AS avg_price, max(1.*price/amount) AS max_price
 FROM shops_history
 WHERE server_id = 3 AND item_id = %s AND created > %s
 GROUP BY operation ORDER BY operation;
--- pyramid/ensemplix/sql/items_stats.sql
+++ pyramid/ensemplix/sql/items_stats.sql
@@ -1,12 +1,12 @@
-SELECT id, title, icon_image, buy_price, sell_price FROM items
+SELECT id, title, icon_image, avg_buy_price, avg_sell_price FROM items
 	LEFT JOIN (
-		SELECT item_id, avg(64.*price/amount) AS buy_price
+		SELECT item_id, 64.*sum(price)/sum(amount) AS avg_buy_price
 		FROM shops_history
 		WHERE NOT operation AND server_id = 3 AND created > %s
 		GROUP BY item_id
 	) AS avg_buy ON avg_buy.item_id = id
 	LEFT JOIN (
-		SELECT item_id, avg(64.*price/amount) AS sell_price
+		SELECT item_id, 64.*sum(price)/sum(amount) AS avg_sell_price
 		FROM shops_history
 		WHERE operation AND server_id = 3 AND created > %s
 		GROUP BY item_id
