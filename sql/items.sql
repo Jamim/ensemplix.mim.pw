11b8f96f8bc591c3161beaf92337b6f7a00c1016
Mim <mimworkmail@gmail.com>
2015-02-23 17:52:40 +0300

    Исправлено добавление предметов с повторяющимися идентификаторами

--- sql/items.sql
+++ sql/items.sql
@@ -1,9 +1,10 @@
 CREATE TABLE items (
-	id		SERIAL NOT NULL PRIMARY KEY,
+	id		SERIAL NOT NULL,
 	data		SERIAL NOT NULL,
 	title		TEXT NOT NULL,
 	icon_image	TEXT
 );
 
+CREATE        INDEX items_id_idx      ON items (id);
 CREATE        INDEX items_data_idx    ON items (data);
 CREATE UNIQUE INDEX items_id_data_idx ON items (id, data);
