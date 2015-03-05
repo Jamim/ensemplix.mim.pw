6ebc2cb0d4d617a724c5828278168e362976b028
Mim <mimworkmail@gmail.com>
2015-03-05 20:06:50 +0300

    Из процедуры получения списка магазинов для проверки вынесен код получения сделок и последних проверок

---
+++ sql/last_deals_ids.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION last_deals_ids(min_time INTEGER)
+RETURNS TABLE(id INTEGER) AS $$
+
+BEGIN
+	RETURN QUERY
+		SELECT max(shops_history.id) AS id
+		FROM shops_history
+		WHERE created > min_time
+		GROUP BY server_id, x, y, z;
+END;
+
+$$ LANGUAGE plpgsql;
--- sql/shops_for_attestation.sql
+++ sql/shops_for_attestation.sql
@@ -11,24 +11,15 @@ BEGIN
 	hour_ago = unix_timestamp - 3600;
 	week_ago = unix_timestamp - 604800;
 
-	CREATE TEMP TABLE last_deals ON COMMIT DROP AS
-		SELECT max(history.id) AS id
-		FROM shops_history AS history
-		WHERE history.created > week_ago
-		GROUP BY history.server_id, history.x, history.y, history.z;
-
-	CREATE TEMP TABLE last_attestation ON COMMIT DROP AS
-		SELECT max(attestation.id) AS id, deal_id
-		FROM shops_attestation AS attestation
-		WHERE deal_id IN (SELECT deals.id FROM last_deals AS deals)
-		GROUP BY deal_id;
+	CREATE TEMP TABLE last_deals (id INTEGER NOT NULL PRIMARY KEY, attestation_id INTEGER) ON COMMIT DROP;
+	CREATE INDEX last_deals_attestation_id_idx ON last_deals (attestation_id);
+	INSERT INTO last_deals SELECT * FROM last_deals_ids_with_attestation_ids(week_ago);
 
 	RETURN QUERY
 		SELECT history.id, history.created, history.server_id, history.x, history.y, history.z
-		FROM last_deals
-			JOIN shops_history AS history ON history.id = last_deals.id
-			LEFT JOIN last_attestation ON last_attestation.deal_id = history.id
-			LEFT JOIN shops_attestation AS attestation ON attestation.id = last_attestation.id
+		FROM last_deals AS deals
+			JOIN shops_history AS history ON history.id = deals.id
+			LEFT JOIN shops_attestation AS attestation ON attestation.id = deals.attestation_id
 		WHERE
 			attestation.reason_id IS NULL AND history.created < hour_ago OR
 			attestation.reason_id = 1 AND attestation.created < hour_ago
