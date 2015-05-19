4fe5272d84788f4c8aabcc5a7a7a5c723099eb61
Mim <mimworkmail@gmail.com>
2015-05-19 22:32:03 +0300

    Реализована возможность ручного добавления одного из серверов в базу данных

--- apps/servers.py
+++ apps/servers.py
@@ -11,9 +11,16 @@ password = argv[1]
 sql_connection = psycopg2.connect("dbname='ensemplix' user='ensemplix' host='localhost' password='%s'" % (password,))
 cursor = sql_connection.cursor()
 
-cursor.executemany("INSERT INTO servers VALUES (%(id)s, %(name)s, %(maximum)s, "
-	"%(ip)s, %(port)s, %(server_type)s, %(border)s, %(world)s,"
-	"%(map)s, %(server_version)s, %(client_version)s);", servers)
+insert_sql = "INSERT INTO servers VALUES (%(id)s, %(name)s, %(maximum)s, " \
+	"%(ip)s, %(port)s, %(server_type)s, %(border)s, %(world)s," \
+	"%(map)s, %(server_version)s, %(client_version)s);"
+server_name = argv[2] if len(argv) == 3 else None
+if server_name:
+	for server in servers:
+		if server['name'] == server_name:
+			cursor.execute(insert_sql, server)
+else:
+	cursor.executemany(insert_sql, servers)
 cursor.close()
 
 sql_connection.commit()
