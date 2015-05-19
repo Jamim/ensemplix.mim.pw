215861b5b19cdb66e6044fc50458b8aeb085aadb
Mim <mimworkmail@gmail.com>
2015-05-19 23:03:08 +0300

    Добавлена обработка отсутствия сервера

--- apps/ensemplix_deals.py
+++ apps/ensemplix_deals.py
@@ -1,6 +1,14 @@
 from ensemplix_log import log
 from ensemplix_players import players_ignore_list
 
+def skip_deal_log(deal, reason):
+	log('Пропущена сделка: \033[0;31m%d\033[0;33m: причина: \033[0;31m%s', deal['id'], reason, style='0;33')
+
+def skip_deal_by_world(deal, world):
+	skip_deal_log(deal, 'мир \033[0;36m%s\033[0;31m не найден' % world)
+
+def skip_deal_by_player(deal, player):
+	skip_deal_log(deal, 'игрок \033[0;36m%s\033[0;31m игнорируется' % player)
 
 def insert_deals(cursor, servers, players, new_deals):
 	if not new_deals:
@@ -8,13 +16,21 @@ def insert_deals(cursor, servers, players, new_deals):
 
 	accepted_deals = []
 	for deal in new_deals:
-		client = deal['from'].lower()
-		owner  = deal['to'].lower()
-		if client in players_ignore_list or owner in players_ignore_list:
-			log('Пропущена сделка: \033[0;31m%d', deal['id'], style='0;33')
+		world = deal['world']
+		if world not in servers:
+			skip_deal_by_world(deal, world)
 			continue
 
-		deal['server_id'] = servers[deal['world']]
+		client = deal['from'].lower()
+		owner  = deal['to'].lower()
+		if client in players_ignore_list:
+			skip_deal_by_player(deal, deal['from'])
+			continue
+		if owner in players_ignore_list:
+			skip_deal_by_player(deal, deal['to'])
+			continue
+
+		deal['server_id'] = servers[world]
 		deal['client']    = players[client]
 		deal['owner']     = players[owner]
 		accepted_deals.append(deal)
